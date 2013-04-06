d3f41d039ea1ae152c6f91b71112109334e2c779
Nurbek Dos <nurbekd@gmail.com>
2013-04-06 10:39:47 +0200

    A file containing function design recipes added

---
+++ Recipe for Designing Functions.py
@@ -0,0 +1,29 @@
+# before creating a code you better make an recipe
+"""
+    1.Examples
+    2.Type Contract
+    3.Header
+    4.Description
+    5.Body
+    6.Test
+"""
+
+
+Examples
+>>> is_palindrome('noon')
+True
+>>> is_palindrome('racecar')
+True
+>>> is_palindrome('dented')
+False
+Type Contract
+(str) --> boolean
+Header
+def is_palindrome(s):
+Description
+Return True if and only if s is a palindrome
+Body
+
+Test
+
+
